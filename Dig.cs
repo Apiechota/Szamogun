c9abc60e1b29743ddd0a5068f03b94c4ed34045f
Ase21 <ase21@marzyciel.pl>
2017-08-21 21:05:48 +0200

    git

---
+++ Dig.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dig : MonoBehaviour {
+	private bool a;
+	public GameObject g1;
+	public int loadProgress=0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Horizontal") != 0||Input.GetAxis ("Vertical") != 0)
+			zastaw ();
+	}
+	public void ustaw (){
+		
+		a = true;
+		StartCoroutine (loadDig ());
+	}
+	public void zastaw()
+	{
+		a = false;
+	}
+
+	public	IEnumerator loadDig () {
+		
+	//	progressBar.SetActive(true);
+		 loadProgress=0;
+		g1.GetComponent<RectTransform> ().localScale = new Vector3 ((float)loadProgress / 100.0f, 1, 1);
+		while(loadProgress<99&&a){
+			yield return new WaitForSeconds(0.1f);
+			loadProgress += 2;
+			g1.GetComponent<RectTransform> ().localScale = new Vector3 ((float)loadProgress / 100.0f, 1, 1);
+
+		}
+		yield return null;
+	}
+
+}
--- GUIeq.cs
+++ GUIeq.cs
@@ -14,52 +14,58 @@ public class GUIeq : MonoBehaviour {
 		
 		if (Input.GetKeyDown (KeyCode.Alpha0)) {
 			frame.transform.position = new Vector3 (-30, 33, 1);
-			player.GetComponent<Atak> ().zastaw ();
+			zastaw ();
 		}
 		if (Input.GetKeyDown (KeyCode.Alpha1)) {
-			player.GetComponent<Atak> ().zastaw ();
+			zastaw ();
 			frame.transform.position = new Vector3 (30, 33, 1);
 
 		}
 		if (Input.GetKeyDown (KeyCode.Alpha2)) {
-			player.GetComponent<Atak> ().zastaw ();
+			zastaw ();
 			frame.transform.position = new Vector3 (90, 33, 1);
 			player.GetComponent<Atak> ().ustaw (true);
 		}
 		if (Input.GetKeyDown (KeyCode.Alpha3)) {
-			player.GetComponent<Atak> ().zastaw ();
+			zastaw ();
 			frame.transform.position = new Vector3 (150, 33, 1);
 			player.GetComponent<Atak> ().ustaw (false);
 		}
 		if (Input.GetKeyDown (KeyCode.Alpha4)) {
-			player.GetComponent<Atak> ().zastaw ();
+			zastaw ();
 			frame.transform.position = new Vector3 (210, 33, 1);
-
+			player.GetComponent<Dig> ().ustaw ();
 		}
 		if (Input.GetKeyDown (KeyCode.Alpha5)) {
-			player.GetComponent<Atak> ().zastaw ();
+			zastaw ();
 			frame.transform.position = new Vector3 (270, 33, 1);
 
 		}
 		if (Input.GetKeyDown (KeyCode.Alpha6)) {
-			player.GetComponent<Atak> ().zastaw ();
+			zastaw ();
 			frame.transform.position = new Vector3 (330, 33, 1);
 
 		}
 		if (Input.GetKeyDown (KeyCode.Alpha7)) {
-			player.GetComponent<Atak> ().zastaw ();
+			zastaw ();
 			frame.transform.position = new Vector3 (390, 33, 1);
 
 		}
 		if (Input.GetKeyDown (KeyCode.Alpha8)) {
-			player.GetComponent<Atak> ().zastaw ();
+			zastaw ();
 			frame.transform.position = new Vector3 (450, 33, 1);
 
 		}
 		if (Input.GetKeyDown (KeyCode.Alpha9)) {
-			player.GetComponent<Atak> ().zastaw ();
+			
 			frame.transform.position = new Vector3 (510, 33, 1);
 
 		}
+	}
+
+	private void zastaw()
+	{
+		player.GetComponent<Atak> ().zastaw ();
+		player.GetComponent<Dig> ().zastaw ();
 	}
 }
